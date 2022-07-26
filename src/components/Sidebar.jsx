d1e4a08c0716a639969d60a96c617deac47e565f
GitJooJ <jpavicari@hotmail.com>
2022-07-26 18:31:07 -0300

    feat: avatar images and styles are now a component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react' 
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -10,8 +11,7 @@ export function Sidebar(){
             src='https://images.unsplash.com/photo-1598887202063-9958501351ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50' />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} 
-                src="https://github.com/GitJooJ.png"/>
+                <Avatar src="https://github.com/GitJooJ.png"/>
 
                 <strong>João Pedro</strong>
                 <span>Web Developer</span>
