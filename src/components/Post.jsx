fa21d6b42841f9746c11df59b241f23b984b2475
GitJooJ <jpavicari@hotmail.com>
2022-08-22 18:25:50 -0300

    feat: functional comments with useState

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,6 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
-import {} from 'date-fns/locale/pt-BR'
+import { useState } from 'react';
 
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
@@ -8,57 +8,69 @@ import styles from './Post.module.css'
 
 
 export function Post({ author, publishedAt, content }){
-    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
-        locale: ptBR,
-    })
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ]);
+  
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  })
 
-    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
-        locale: ptBR,
-        addSuffix: true,
-    })
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
 
-    return(
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <Avatar src={author.avatar_url} />
-                    <div className={styles.authorInfo}>
-                        <strong>{author.name}</strong>
-                        <span>{author.role}</span>
-                    </div>
-                </div>
+  function handleCreateNewComment(){
+    event.preventDefault();
 
-                <time title="{publishedDateFormatted}" dateTime={publishedAt.toISOString()}>
-                {publishedDateRelativeToNow}
-                </time>
-            </header>
+    setComments([...comments, comments.length + 1]);
+  }
+
+  return(
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatar_url} />
+          <div className={styles.authorInfo}>
+              <strong>{author.name}</strong>
+              <span>{author.role}</span>
+          </div>
+        </div>
+
+        <time title="{publishedDateFormatted}" dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
         
-            <div className={styles.content}>
-                {content.map(line => {
-                    if (line.type == 'paragraph') {
-                        return <p>{line.content}</p>
-                    } else if (line.type == 'link') {
-                        return <p><a href="#">{line.content}</a></p>;
-                    }
-                })}
-            </div>
+      <div className={styles.content}>
+        {content.map(line => {
+          if (line.type == 'paragraph') {
+            return <p>{line.content}</p>
+          } else if (line.type == 'link') {
+              return <p><a href="#">{line.content}</a></p>;
+            }
+          })}
+      </div>
 
-            <form className={styles.commentForm}>
-                <strong>Deixe seu feedback</strong>
+        <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+          <strong>Deixe seu feedback</strong>
 
-                <textarea
-                    placeholder="Deixe um comentário"
-                />
-                <footer>
-                    <button type="submit">Publicar</button>
-                </footer>
-            </form>
+            <textarea
+              placeholder="Deixe um comentário"
+            />
+            
+          <footer>
+            <button type="submit">Publicar</button>
+          </footer>
+        </form>
 
-            <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
-            </div>
-        </article>
+          <div className={styles.commentList}>
+            {comments.map(comment => {
+              return <Comment />
+            })}
+          </div>
+      </article>
     );
 }
